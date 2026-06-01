cbf4ac2ae6e61c2c74fb2fd1c4410d1e1e9639c5
dmitry.sankovsky <dmitry.sankovsky@kufar.by>
2026-06-01 20:10:50 +0300

    Initial commit

---
+++ sharedLogic/src/commonMain/kotlin/com/dsankovsky/kmpclientplanner/Greeting.kt
@@ -0,0 +1,9 @@
+package com.dsankovsky.kmpclientplanner
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return sayHello(platform.name)
+    }
+}
---
+++ sharedLogic/src/commonMain/kotlin/com/dsankovsky/kmpclientplanner/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.dsankovsky.kmpclientplanner
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
