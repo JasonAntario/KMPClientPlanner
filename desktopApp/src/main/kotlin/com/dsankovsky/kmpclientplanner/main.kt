3897db2f9b2961d32e38e0faf00943e23b47aeb0
dmitry.sankovsky <dmitry.sankovsky@kufar.by>
2026-06-24 21:32:34 +0300

    Revert "new design part 1"
    This reverts commit e786cbbbf870455953a627a105e0c29ec9033f28.

--- desktopApp/src/main/kotlin/com/dsankovsky/kmpclientplanner/main.kt
+++ desktopApp/src/main/kotlin/com/dsankovsky/kmpclientplanner/main.kt
@@ -1,15 +1,11 @@
 package com.dsankovsky.kmpclientplanner
 
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
 import com.dsankovsky.kmpclientplanner.di.platformModule
 import com.dsankovsky.kmpclientplanner.di.repositoryModule
 import com.dsankovsky.kmpclientplanner.di.uiModule
 import com.dsankovsky.kmpclientplanner.di.useCasesModule
-import com.dsankovsky.kmpclientplanner.uinew.desktop.DesktopApp
-import com.dsankovsky.kmpclientplanner.uinew.desktop.theme.LessonsDesktopTheme
 import org.koin.core.context.startKoin
 
 fun main() {
@@ -17,15 +13,11 @@ fun main() {
         modules(platformModule, useCasesModule, repositoryModule, uiModule)
     }
     application {
-        val windowState = rememberWindowState(width = 1180.dp, height = 760.dp)
         Window(
             onCloseRequest = ::exitApplication,
-            state = windowState,
-            title = "Lessons",
+            title = "Client Planner",
         ) {
-            LessonsDesktopTheme {
-                DesktopApp()
-            }
+            App()
         }
     }
 }
